b77cd1bd9fbaba0944b9474fb35c0fdfa7bc02d2
Alice Zou <25268163+Alicehappy@users.noreply.github.com>
2024-07-28 16:01:22 -0400

    Add the categoroy description

---
+++ src/app/opti-card/card-description.enum.ts
@@ -0,0 +1,29 @@
+enum CategoryDescription {
+    DrugStore = 'Drug Store',
+    Entertainment = 'Entertainment',
+    Furniture = 'Furniture',
+    Gas = 'Gas',
+    Groceries = 'Groceries',
+    HomeImprovement = 'Home Improvement',
+    Hotel = 'Hotel/Motel',
+    Parking = 'Parking/Public Transit/Rides',
+    RecurringBills = 'Recurring Bills',
+    Restaurants = 'Restaurants/Dining',
+    Streaming = 'Streaming/Digital Subscriptions',
+    Travel = 'Travel',
+    Other = 'Other',
+}
+
+
+
+      
+      
+      
+      
+      
+        
+      
+      
+      
+      
+     
